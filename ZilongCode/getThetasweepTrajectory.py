4a480625a5ae7ea301ad5e1d3d1674ce5b6da37f
ZilongJi <jzlxmu@163.com>
2023-09-10 13:26:30 +0100

    update code

--- ZilongCode/getThetasweepTrajectory.py
+++ ZilongCode/getThetasweepTrajectory.py
@@ -3,7 +3,7 @@ import os
 import pdb
 from loren_frank_data_processing import (make_epochs_dataframe,
                                          make_neuron_dataframe)
-from src.parameters import (ANIMALS, MAX_N_EXPOSURES, MIN_N_NEURONS)
+from src.parameters import (ANIMALS, MIN_N_NEURONS, _BRAIN_AREAS)
 
 from scripts.run_by_epoch import clusterless_thetasweeps
 from tqdm.auto import tqdm
@@ -11,6 +11,12 @@ from tqdm.auto import tqdm
 def main():
     epoch_info = make_epochs_dataframe(ANIMALS)
     neuron_info = make_neuron_dataframe(ANIMALS)
+    
+    euron_info = neuron_info.loc[
+    (neuron_info.type == 'principal') &
+    (neuron_info.numspikes > 100) &
+    neuron_info.area.isin(_BRAIN_AREAS)]
+    
     n_neurons = (neuron_info
                     .groupby(['animal', 'day', 'epoch'])
                     .neuron_id
@@ -21,8 +27,7 @@ def main():
     epoch_info = epoch_info.join(n_neurons)
     is_w_track = (epoch_info.environment
                     .isin(['TrackA', 'TrackB', 'WTrackA', 'WTrackB']))
-    #is_animal = epoch_info.index.isin(['bon', 'fra', 'gov', 'dud', 'con'], level='animal')
-    is_animal = epoch_info.index.isin(['bon', 'cha', 'con'], level='animal')
+    is_animal = epoch_info.index.isin(['bon', 'fra', 'gov', 'dud', 'con'], level='animal')
     
     valid_epochs = (is_w_track &
                     (epoch_info.n_neurons > MIN_N_NEURONS) &
@@ -45,7 +50,11 @@ def main():
             continue # skip this epoch because there is some error in the data will come back to this later
         if animal == 'cha' and day == 3 and epoch == 4:
             continue # skip this epoch because there is some error in the data will come back to this later
-        
+        if animal == 'dud' and day == 3 and epoch == 2:
+                continue # skip this epoch because there is some error in the data will come back to this later
+        if animal == 'dud' and day == 3 and epoch == 4:
+            continue # skip this epoch because there is some error in the data will come back to this later
+                    
         # Check if this file has already been run
         replay_info_filename = os.path.join(
             DATA_DIR,
@@ -55,13 +64,11 @@ def main():
         if not os.path.isfile(replay_info_filename):
             animal, day, epoch = epoch_key
             print(f'Animal: {animal}, Day: {day}, Epoch: {epoch}')
-            clusterless_thetasweeps(epoch_key) #set brain_areas to ['CA1'] if only care about CA1
-            
-    # %% run single epoch_key
-    # PROCESSED_DATA_DIR = '/home/zilong/Desktop/replay_trajectory_paper/ThetaSweep-Results'
-    # epoch_key = ("bon", 8, 4)
-    # print(f'Animal: {epoch_key[0]}, Day: {epoch_key[1]}, Epoch: {epoch_key[2]}')
-    # clusterless_thetasweeps(epoch_key) 
+            clusterless_thetasweeps(epoch_key,
+                                    exclude_interneuron_spikes=True,
+                                    brain_areas=None,
+                                    save_original_data=False) 
+
 
 if __name__ == '__main__':
     main()
--- ZilongCode/runall_zl_clusterless.py
+++ ZilongCode/runall_zl_clusterless.py
@@ -3,7 +3,7 @@ import os
 import pdb
 from loren_frank_data_processing import (make_epochs_dataframe,
                                          make_neuron_dataframe)
-from src.parameters import (ANIMALS, MAX_N_EXPOSURES, MIN_N_NEURONS)
+from src.parameters import (ANIMALS, MIN_N_NEURONS, _BRAIN_AREAS)
 
 from scripts.run_by_epoch import clusterless_analysis_1D, sorted_spikes_analysis_1D
 from tqdm.auto import tqdm
@@ -11,6 +11,12 @@ from tqdm.auto import tqdm
 def main():
     epoch_info = make_epochs_dataframe(ANIMALS)
     neuron_info = make_neuron_dataframe(ANIMALS)
+    
+    neuron_info = neuron_info.loc[
+    (neuron_info.type == 'principal') &
+    (neuron_info.numspikes > 100) &
+    neuron_info.area.isin(_BRAIN_AREAS)]
+    
     n_neurons = (neuron_info
                     .groupby(['animal', 'day', 'epoch'])
                     .neuron_id
@@ -19,15 +25,17 @@ def main():
                     .to_frame())
 
     epoch_info = epoch_info.join(n_neurons)
+    
     is_w_track = (epoch_info.environment
                     .isin(['TrackA', 'TrackB', 'WTrackA', 'WTrackB']))
-    #is_animal = epoch_info.index.isin(['bon', 'fra', 'gov', 'dud', 'con'], level='animal')
-    is_animal = epoch_info.index.isin(['bon'], level='animal')
+    
+    is_animal = epoch_info.index.isin(['bon', 'fra', 'gov', 'dud', 'con'], level='animal')
+
     valid_epochs = (is_w_track &
                     (epoch_info.n_neurons > MIN_N_NEURONS) &
-                    (epoch_info.exposure <= MAX_N_EXPOSURES) &
                     is_animal
                     )
+    
     log_directory = os.path.join(os.getcwd(), 'logs')
     os.makedirs(log_directory,  exist_ok=True)
 
@@ -37,16 +45,30 @@ def main():
     PROCESSED_DATA_DIR = '/home/zilong/Desktop/replay_trajectory_paper/Processed-Data'
     for epoch_key in tqdm(epoch_info[valid_epochs].index, desc='epochs'):
         animal, day, epoch = epoch_key
+        
+        if animal == 'cha' and day == 3 and epoch == 2:
+            continue # skip this epoch because there is some error in the data will come back to this later
+        if animal == 'cha' and day == 3 and epoch == 4:
+            continue # skip this epoch because there is some error in the data will come back to this later
+        if animal == 'dud' and day == 3 and epoch == 2:
+            continue # skip this epoch because there is some error in the data will come back to this later
+        if animal == 'dud' and day == 3 and epoch == 4:
+            continue # skip this epoch because there is some error in the data will come back to this later
+                               
+        
         # Check if this file has already been run
         replay_info_filename = os.path.join(
             PROCESSED_DATA_DIR,
-            f'{animal}_{day:02d}_{epoch:02d}_clusterless_1D_replay_info_80.csv')
+            'ReplayTrajectories',
+            f'{animal}_{day:02d}_{epoch:02d}_clusterless_1D_no_interneuron_replay_info_80.csv')
         if not os.path.isfile(replay_info_filename):
             #pdb.set_trace()
             animal, day, epoch = epoch_key
             print(f'Animal: {animal}, Day: {day}, Epoch: {epoch}')
             #pdb.set_trace()
-            clusterless_analysis_1D(epoch_key, brain_areas=None) #set brain_areas to ['CA1'] if only care about CA1
+            clusterless_analysis_1D(epoch_key, 
+                                    exclude_interneuron_spikes=True,
+                                    brain_areas=None) #set brain_areas to ['CA1'] if only care about CA1
     # %%
 
 if __name__ == '__main__':
